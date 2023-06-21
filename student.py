7e7d0f75850510a8e513dda103a447128f07abee
GenaGrig <gennadiuss@gmail.com>
2023-06-21 17:04:32 +0000

    Unittest Framework and code

---
+++ student.py
@@ -0,0 +1,32 @@
+from datetime import date, timedelta
+import requests
+
+class Student:
+    def __init__(self, first_name, last_name, birthday):
+        self._first_name = first_name
+        self._last_name = last_name
+        self.birthday = birthday
+        self._start_date = date.today()
+        self.end_date = self._start_date + timedelta(days=365)
+        self.naughty_list = False
+    
+  
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+    
+    def alert_santa(self):
+        self.naughty_list = True
+        
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+    
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+        
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+        if response.ok:
+            return response.text
+        return "Something went wrong with the request!"
